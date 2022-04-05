881b106a959af44dc52b8f06bc5551d72ae95849
royjin1 <103071640+royjin1@users.noreply.github.com>
2022-04-05 16:28:14 -0400

    Update society_hierarchy.py

--- a2/society_hierarchy.py
+++ a2/society_hierarchy.py
@@ -1,7 +1,7 @@
 """Assignment 2: Society Hierarchy (all tasks)
 
 CSC148, Winter 2022
-123
+
 This code is provided solely for the personal and private use of students
 taking the CSC148 course at the University of Toronto. Copying for purposes
 other than this use is expressly prohibited. All forms of distribution of this
